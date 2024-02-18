0f35df2157d6cca4a87f7248c62118496e941342
Alsona Natsi <alsona.naci1188@gmail.com>
2024-02-18 02:06:38 +0000

    Creating a new file with question.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,3 @@
-// Adding a mixed array with objects
-// Adding 10 questions for the quiz
-const questions = [
-    {
-        question: "Where will EURO 2024 be held?",
-        answers: [
-            { option: "Greece", value: false },
-            { option: "Italy", value: false },
-            { option: "Germany", value: true },
-            { option: "USA", value: false },
-        ]
-    },
-
-    {
-        question: "When is EURO 2024 scheduled to take place?",
-        answers: [
-            { option: "14 June - 30 June 2024", value: false },
-            { option: "14 June - 5 July 2024", value: false },
-            { option: "14 June - 15 July 2024", value: false },
-            { option: "14 June - 14 July 2024", value: true },
-        ]
-    },
-    {
-        question: "Which from these countries is not qualified for the EURO 2024?",
-        answers: [
-            { option: "Albania", value: false },
-            { option: "Croatia", value: false },
-            { option: "Netherlands", value: false },
-            { option: "Romania", value: true },
-        ]
-    },
-    {
-        question: "Where will the final take place?",
-        answers: [
-            { option: "BVB Stadion Dortmund", value: false },
-            { option: "Düsseldorf Arena", value: false },
-            { option: "Olympiastadion Berlin", value: true },
-            { option: "Frankfurt Arena", value: false },
-        ]
-    },
-    {
-        question: "Which country won EURO 2008 cup?",
-        answers: [
-            { option: "Italy", value: false },
-            { option: "Spain", value: true },
-            { option: "Portugal", value: false },
-            { option: "England", value: false },
-        ]
-    },
-    {
-        question: "How many times has Germany won the EURO cup?",
-        answers: [
-            { option: "5", value: false },
-            { option: "4", value: false },
-            { option: "3", value: true },
-            { option: "2", value: false },
-        ]
-    },
-    {
-        question: "When and where was held the first European Football Championship?",
-        answers: [
-            { option: "Paris, 1960", value: true },
-            { option: "London, 1954", value: false },
-            { option: "Rome, 1956", value: false },
-            { option: "Brussels, 1958", value: false },
-        ]
-    },
-    {
-        question: "Which from these countries never won the European Championship?",
-        answers: [
-            { option: "Greece", value: false },
-            { option: "Denmark", value: false },
-            { option: "Russia", value: false },
-            { option: "Belgium", value: true },
-        ]
-    },
-    {
-        question: "In 1968 Italy won the final match against which country?",
-        answers: [
-            { option: "England", value: false },
-            { option: "Soviet Union", value: false },
-            { option: "Yugoslavia", value: true },
-            { option: "Hungary", value: false },
-        ]
-    },
-    {
-        question: "How many nations compete at the European Championship?",
-        answers: [
-            { option: "20", value: false },
-            { option: "22", value: false },
-            { option: "26", value: false },
-            { option: "24", value: true },
-        ]
-    },
-];
-
 // initiating these variables for all elements's id 
 const userInput = document.getElementById('user-input');
 const welcomeSection = document.getElementById('welcome-section');
@@ -300,8 +204,8 @@ nextButton.addEventListener("click", () => {
         currentQuestionIndex = 0;
         score = 0;
         // Hide the next button
-        nextButton.style.display = "none";
-        // Remove the welcome message
+       
+        // Clear the welcome message
         welcomeSection.innerHTML = '';
         // Show the input field, submit button, and name label again
         userInput.style.display = 'block';
